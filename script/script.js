c09cf92bb5ddc7722c3426543474eddd723d6f06
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2024-03-05 16:34:58 -0300

    ..

--- script/script.js
+++ script/script.js
@@ -1,5 +1,20 @@
-const addressForm = document.querySelector("#address-form")
-const cepInput = document.querySelector("#cep")
-const addressInput = document.querySelector("#address")
-const cityInput = document.querySelector("#city")
-const neighboorhoodInput = document.querySelector("#neighborhood")
+const addressForm = document.querySelector("#address-form");
+const cepInput = document.querySelector("#cep");
+const addressInput = document.querySelector("#address");
+const cityInput = document.querySelector("#city");
+const neighboorhoodInput = document.querySelector("#neighborhood");
+const regionInput = document.querySelector("#region");
+const formInputs = document.querySelectorAll("[data-input]");
+
+const closeButton = document.querySelector("#close-message");
+
+// Validate CEP input
+cepInput.addEventListener("Keypress", (e) => {
+    const onlyNumbers = /[0-9]|\./;
+    const key = String.fromCharCode(e.keyCode);
+
+    if(!onlyNumbers.test(key)) {
+        e.preventDefault();
+        return;
+    }
+})
